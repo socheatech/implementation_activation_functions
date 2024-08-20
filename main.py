1d6e1f69cf6799238282a2f28280e34ccaa21b6f
Sochea <socheatech@gmail.com>
2024-08-20 18:18:38 +0700

    update rang[-1, 0, 1]

--- main.py
+++ main.py
@@ -2,69 +2,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Sigmoid:
+# Sigmoid function
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-# ReLU
+# ReLU function
 def relu(x):
     return np.maximum(0, x)
 
 
-# Tanh
+# Tanh function
 def tanh(x):
     return np.tanh(x)
 
 
-# Softmax (usually applied to a vector of values)
+# Softmax function
 def softmax(x):
     exp_x = np.exp(x)
-    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    return exp_x / np.sum(exp_x, axis=0)  # Sum over the entire array
 
 
-# Leaky ReLU
+# Leaky ReLU function
 def leaky_relu(x, alpha=0.01):
     return np.maximum(alpha * x, x)
 
 
-# Generate input data
-x = np.linspace(-5, 5, 400)
-x_matrix = x.reshape(-1, 1)  # For softmax (which usually expects 2D input)
+# Define the input values
+x_values = np.array([-1, 0, 1])
 
-# Plotting the activation functions
-plt.figure(figsize=(12, 8))
+sigmoid_values = sigmoid(x_values)
+relu_values = relu(x_values)
+tanh_values = tanh(x_values)
+softmax_values = softmax(x_values)
+leaky_relu_values = leaky_relu(x_values)
+
+# Plotting the activation functions using line plots
+plt.figure(figsize=(12, 6))
 
 # Sigmoid
 plt.subplot(2, 3, 1)
-plt.plot(x, sigmoid(x), label="Sigmoid", color='blue')
+plt.plot(x_values, sigmoid_values, marker='o', color='blue')
 plt.title("Sigmoid")
 plt.grid()
 
 # ReLU
 plt.subplot(2, 3, 2)
-plt.plot(x, relu(x), label="ReLU", color='red')
+plt.plot(x_values, relu_values, marker='o', color='red')
 plt.title("ReLU")
 plt.grid()
 
 # Tanh
 plt.subplot(2, 3, 3)
-plt.plot(x, tanh(x), label="Tanh", color='green')
+plt.plot(x_values, tanh_values, marker='o', color='green')
 plt.title("Tanh")
 plt.grid()
 
-# Softmax (since it’s typically applied to multiple values)
+# Softmax
 plt.subplot(2, 3, 4)
-plt.plot(x, softmax(x_matrix).flatten(), label="Softmax", color='purple')
+plt.plot(x_values, softmax_values, marker='o', color='purple')
 plt.title("Softmax")
 plt.grid()
 
 # Leaky ReLU
 plt.subplot(2, 3, 5)
-plt.plot(x, leaky_relu(x), label="Leaky ReLU", color='orange')
+plt.plot(x_values, leaky_relu_values, marker='o', color='orange')
 plt.title("Leaky ReLU")
 plt.grid()
 
-# Adjust layout
 plt.tight_layout()
 plt.show()
